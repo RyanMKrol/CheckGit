45180fcdc0e1be2920ef3f34d7a1c82bf3e11372
RyanMKrol <ryankrol.m@gmail.com>
2020-12-08 19:03:33 +0000

    Add module for reading paths

---
+++ src/modules/read.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import util from 'util';
+
+import { info, error, PATHS_STORAGE_FILENAME } from './constants';
+
+const open = util.promisify(fs.open);
+const readFile = util.promisify(fs.readFile);
+
+/**
+ * Reads the paths that are stored locally
+ *
+ */
+async function readPaths() {
+  info('Reading the current paths in storage');
+
+  return open(PATHS_STORAGE_FILENAME, 'a+')
+    .then(async (fd) => {
+      const readData = await readFile(fd);
+      const currentPaths = readData.length > 0 ? JSON.parse(readData) : [];
+
+      info(`Current paths: ${currentPaths}`);
+
+      return currentPaths;
+    })
+    .catch((e) => {
+      error('Failed to read, with error: %O', e);
+      throw new Error(`Could not open the file for reading ${e.toString()}`);
+    });
+}
+
+export default readPaths;
