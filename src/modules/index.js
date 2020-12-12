1847e30db6626dbe16adb6dc45aaa853f2d43864
RyanMKrol <ryankrol.m@gmail.com>
2020-12-12 20:11:36 +0000

    Add modules index

---
+++ src/modules/index.js
@@ -0,0 +1,10 @@
+export { default as checkPaths } from './check';
+export { default as addPath } from './add';
+export { default as removePath } from './remove';
+export {
+  printPath,
+  printRemoveError,
+  printRemoveSuccess,
+  printAddError,
+  printAddSuccess,
+} from './messaging';
