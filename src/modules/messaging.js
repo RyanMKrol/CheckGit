2e4974bacee276e170d9b3eadc4dba0a832b4dfd
RyanMKrol <ryankrol.m@gmail.com>
2020-12-12 20:01:39 +0000

    Add addition messaging

--- src/modules/messaging.js
+++ src/modules/messaging.js
@@ -15,4 +15,38 @@ function printPath(path) {
   );
 }
 
-export default printPath;
+/**
+ * Util for printing when we fail to remove an item from storage
+ *
+ * @param {string} message Any custom error message to write out
+ */
+function printRemoveError(message) {
+  console.log(chalk.bold.red(`Failed to remove item - ${message}`));
+}
+
+/**
+ * Util for printing when we succeed in removing an item from storage
+ */
+function printRemoveSuccess() {
+  console.log(chalk.bold.green('Removed path successfully!'));
+}
+
+/**
+ * Util for printing when we fail to add an item to storage
+ *
+ * @param {string} message Any custom error message to write out
+ */
+function printAddError(message) {
+  console.log(chalk.bold.red(`Failed to add item - ${message}`));
+}
+
+/**
+ * Util for printing when we succeed in adding an item to storage
+ */
+function printAddSuccess() {
+  console.log(chalk.bold.green('Added path successfully!'));
+}
+
+export {
+  printPath, printRemoveError, printRemoveSuccess, printAddError, printAddSuccess,
+};
