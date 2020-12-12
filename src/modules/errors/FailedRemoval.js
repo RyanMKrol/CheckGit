965292759bcfc283dd41f8ea1f7797d866826637
RyanMKrol <ryankrol.m@gmail.com>
2020-12-12 20:02:31 +0000

    Add FailedRemoval error module

---
+++ src/modules/errors/FailedRemoval.js
@@ -0,0 +1,22 @@
+/**
+ * Error used when we failed to remove an item
+ */
+class FailedRemoval extends Error {
+  /**
+   * @param {...any} params
+   */
+  constructor(...params) {
+    // Pass remaining arguments (including vendor specific ones) to parent constructor
+    super(...params);
+
+    // Maintains proper stack trace for where our error was thrown (only available on V8)
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, FailedRemoval);
+    }
+
+    this.Error = 'FailedRemoval';
+    this.StatusCode = 500;
+  }
+}
+
+export default FailedRemoval;
