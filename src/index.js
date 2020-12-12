5e8c5a60b4b00c092ef413fc4363d873eb7de713
RyanMKrol <ryankrol.m@gmail.com>
2020-12-12 20:20:36 +0000

    Update index file with CLI options

--- src/index.js
+++ src/index.js
@@ -1,5 +1,47 @@
 #!/usr/bin/env node
 
-import customFunction from './module';
+import yargs from 'yargs';
+import {
+  addPath,
+  checkPaths,
+  removePath,
+  printCheckPath,
+  printCheckError,
+  printRemoveError,
+  printRemoveSuccess,
+  printAddError,
+  printAddSuccess,
+} from './modules';
 
-customFunction();
+/**
+ * Main
+ */
+async function main() {
+  if (yargs.argv.add) {
+    addPath(yargs.argv.add)
+      .then(() => {
+        printAddSuccess();
+      })
+      .catch((error) => {
+        printAddError(error.message);
+      });
+  } else if (yargs.argv.remove) {
+    removePath(yargs.argv.remove)
+      .then(() => {
+        printRemoveSuccess();
+      })
+      .catch((error) => {
+        printRemoveError(error.message);
+      });
+  } else {
+    checkPaths()
+      .then((paths) => {
+        paths.forEach((element) => printCheckPath(element));
+      })
+      .catch((error) => {
+        printCheckError(error.message);
+      });
+  }
+}
+
+main();
--- src/module.js
+++
@@ -1,6 +0,0 @@
-/**
- * A description of a function
- */
-export default function customFunction() {
-  console.log('Hello World!');
-}
